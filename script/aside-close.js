83fea7a88ff69a096e66b9832ec2a828b564b394
Egor <ruxlov99@gmail.com>
2026-07-17 16:10:24 +0300

    Create aside-close.js

---
+++ script/aside-close.js
@@ -0,0 +1,1 @@
+const aside=document.querySelector('[data-header="aside"]'),closeBtn=aside.querySelector('[data-header="aside-close"]');closeBtn.addEventListener("click",()=>{aside.classList.add("close")});
